c456df70eaab43ecb2734947fec76c990e4dd748
nimairoy <nimairoy718@gmail.com>
2023-05-14 13:30:49 +0600

    server upload

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,8 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send('Server is Running');
+app.get('/', (req, res) => {
+  res.send('Server is Running');
 })
 
 
@@ -25,15 +26,114 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+/////////// verify jwt token ////////////
+const verifyJwt = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=>{
+      if(error){
+        return res.status(401).send({error: true, message: 'Unauthorized access'})
+      }
+      req.decoded = decoded;
+      next()
+    })
+}
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const carCollection = client.db("carDoctorDB").collection("car");
+    const checkoutCollection = client.db("carDoctorDB").collection("checkout")
+
+    // jwt json web token create
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      console.log({ token })
+      res.send({ token });
+    })
+
+    // read or get data from database
+    app.get('/services', async (req, res) => {
+      const cursor = carCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    //read or get single data 
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { title: 1, img: 1, service_id: 1, price: 1 }
+      }
+      const result = await carCollection.findOne(query, options);
+      res.send(result);
+    })
 
 
-    
+    /**checkout start */
 
+    // get data from checkout collection 
+    app.get('/checkouts', verifyJwt, async (req, res) => {
+      const decoded = req.decoded;
+      console.log('come back after jwt verify', decoded)
+      // console.log(req.headers.authorization)
+
+      if(decoded.email !== req.query.email){
+        return res.status(403).send({error: 1, message: 'forbidden access'})
+      }
+
+
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await checkoutCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    // create data or post method
+    app.post('/checkouts', async (req, res) => {
+      const checkout = req.body;
+      console.log(checkout)
+      const result = await checkoutCollection.insertOne(checkout);
+      res.send(result);
+    })
+
+    // update checkout pages
+    app.patch('/checkouts/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateCheckouts = req.body;
+      console.log(updateCheckouts)
+      const updateDoc = {
+        $set: {
+          status: updateCheckouts.status,
+        }
+      }
+      const result = await checkoutCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    // delete checkout list
+    app.delete('/checkouts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await checkoutCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    /**checkout end */
 
 
 
@@ -49,6 +149,6 @@ run().catch(console.dir);
 
 
 
-app.listen(port, ()=>{
-    console.log('Server is Running on port: ', port);
+app.listen(port, () => {
+  console.log('Server is Running on port: ', port);
 })
